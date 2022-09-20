c6099953ed7dbb29cd45c6c733aa2d1b43e5d0a0
Oijen <k.c.w.v.oijen@student.tue.nl>
2022-09-20 15:20:05 -0400

    swapped average and sum

--- src/main/java/badcode/BadCode.java
+++ src/main/java/badcode/BadCode.java
@@ -4,14 +4,14 @@ public class BadCode {
 
     // Print the average and the sum of an array of integers
     public int PRINTsumandAVERAGE(int x[]) {
-        int sum;
-        int average = 0;
+        int sum = 0;
+        int average;
 
         for (int i = 0; i < x.length; i++) {
-            average += x[i];
+            sum += x[i];
         }
 
-        sum = average / x.length;
+        average = sum / x.length;
 
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
